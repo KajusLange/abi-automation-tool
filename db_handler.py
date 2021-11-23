ca0e61feb605b1b98c52babb99e2138056c2ea0b
Kajus Lange <kajus.lange@gmail.com>
2021-11-23 08:20:44 +0100

    #FEATURE fetch abiturfach kombination depending on input

--- db_handler.py
+++ db_handler.py
@@ -2,3 +2,15 @@
 import sqlite3
 
 con = sqlite3.connect("data/abitur-automation.sql")
+cur = con.cursor()
+
+
+def fetch_abiturfach_kombination(kurse_kombi):
+    sql = """SELECT kombination_id FROM tbl_abiturfach_kombination 
+    WHERE abiturfach_1=? AND abiturfach_2=? AND abiturfach_3=? AND abiturfach_4=?"""
+
+    cur.execute(sql, kurse_kombi)
+
+    return cur.fetchone()[0]
+
+
