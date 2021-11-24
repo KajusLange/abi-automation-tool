e755947bee772e24770d65c781251b5b892ff5a7
Kajus Lange <kajus.lange@gmail.com>
2021-11-24 17:33:49 +0100

    #CHANGE removed white space between sql querry and execution

--- db_handler.py
+++ db_handler.py
@@ -8,7 +8,6 @@ cur = con.cursor()
 def fetch_abiturfach_kombination(kurse_kombi):
     sql = """SELECT kombination_id FROM tbl_abiturfach_kombination 
     WHERE abiturfach_1=? AND abiturfach_2=? AND abiturfach_3=? AND abiturfach_4=?"""
-
     cur.execute(sql, kurse_kombi)
 
     return cur.fetchone()[0]
@@ -16,11 +15,9 @@ def fetch_abiturfach_kombination(kurse_kombi):
 
 def add_kurs(attributes):
     sql = "INSERT INTO tbl_kurs (None, ?, ?, ?, ?)"
-
     cur.execute(sql, attributes)
 
 
 def add_student(attributes):
     sql = "INSERT INTO tbl_schueler (None, ?, ?, ?, ?, ?)"
-
     cur.execute(sql, attributes)
