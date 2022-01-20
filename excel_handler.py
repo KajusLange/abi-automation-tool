845b04ee3330ea680b7261ea872b33ac61eb2294
Kajus Lange <kjs.lng@protonmail.com>
2022-01-20 13:38:51 +0100

    #FEAT adjust_row_count depending on group size

--- excel_handler.py
+++ excel_handler.py
@@ -3,3 +3,10 @@ from openpyxl import load_workbook
 
 wb = load_workbook(filename="template_excel.xlsx")
 ws = wb.active
+
+
+def adjust_row_count(g_one, g_two, g_three, g_four):
+    ws.row_dimensions.group(12 + g_one, 21, hidden=True)
+    ws.row_dimensions.group(25 + g_two, 34, hidden=True)
+    ws.row_dimensions.group(38 + g_three, 47, hidden=True)
+    ws.row_dimensions.group(51 + g_four, 65, hidden=True)
