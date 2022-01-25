e45f606a43c614173d2f84234e2e019b67277e30
Kajus Lange <kjs.lng@protonmail.com>
2022-01-25 01:31:27 +0100

    #FEAT saving student .xlsx with basic student information

--- excel_handler.py
+++ excel_handler.py
@@ -5,11 +5,16 @@ wb = load_workbook(filename="template_excel.xlsx")
 ws = wb.active
 
 
-def adjust_row_count(g_one, g_two, g_three, g_four):
-    ws.row_dimensions.group(12 + g_one, 21, hidden=True)
-    ws.row_dimensions.group(25 + g_two, 34, hidden=True)
-    ws.row_dimensions.group(38 + g_three, 47, hidden=True)
-    ws.row_dimensions.group(51 + g_four, 65, hidden=True)
+def create_student_xlsx(student):
+    # student info
+    ws["B70"] = student.klasse
+    ws["D70"] = student.name
+    ws["G70"] = student.vorname
+    ws["D73"] = student.pb
+    ws["G73"] = student.wdh
+
+    # final step
+    wb.save(filename=f"{student.klasse}_{student.name}_{student.vorname}")
 
 
 def adjust_row_count(groups):
