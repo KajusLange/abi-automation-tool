c0bc91eaee25683f31868fc5c72bca17f9a7bf84
Kajus Lange <kjs.lng@protonmail.com>
2022-01-25 01:41:11 +0100

    #FEAT Course class

--- data_handler.py
+++ data_handler.py
@@ -7,3 +7,10 @@ class Student:
         self.pb = pb
         self.wdh = wdh
         self.kurse = []
+
+
+class Course:
+    def __init__(self, id, abi, points):
+        self.id = id
+        self.abi = abi
+        self.points = points
