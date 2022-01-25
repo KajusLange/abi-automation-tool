97939c41941bd27402417a311ce60cc213a4fe40
Kajus Lange <kjs.lng@protonmail.com>
2022-01-25 01:31:02 +0100

    #FEAT Hide rows in .xlsx that are not needed

--- excel_handler.py
+++ excel_handler.py
@@ -10,3 +10,12 @@ def adjust_row_count(g_one, g_two, g_three, g_four):
     ws.row_dimensions.group(25 + g_two, 34, hidden=True)
     ws.row_dimensions.group(38 + g_three, 47, hidden=True)
     ws.row_dimensions.group(51 + g_four, 65, hidden=True)
+
+
+def adjust_row_count(groups):
+    ws.row_dimensions.group(12 + groups[0], 21, hidden=True)
+    ws.row_dimensions.group(25 + groups[1], 34, hidden=True)
+    ws.row_dimensions.group(38 + groups[2], 47, hidden=True)
+    ws.row_dimensions.group(51 + groups[3], 65, hidden=True)
+
+    wb.save("debug.xlsx")
